269f008aaf5c3c0b9f917a7dd20875a8bd2acb0e
Yip Cubed <yipcubed@gmail.com>
2019-07-08 17:44:25 -1000

    starting

--- main.cpp
+++ main.cpp
@@ -42,21 +42,22 @@ static auto __ __attribute__((unused)) = []() {              // NOLINT
 
 class Solution {
 public:
-    vector<int> fairCandySwap(vector<int>& A, vector<int>& B) {
+    vector<int> fairCandySwap(vector<int> &A, vector<int> &B) {
         vector<int> ex;
         set<int> inB;
         int suma = 0;
         int sumb = 0;
-        for (auto& i : A) suma += i;
-        for (auto& i : B) {
+        for (auto &i : A) 
+            suma += i;
+        for (auto &i : B) {
             inB.insert(i);
             sumb += i;
         }
         int need = (suma + sumb) / 2 - suma;
-        for (int i = 0; i < A.size(); ++i) {
-            int toget = need + A[i];
+        for (auto i: A) {
+            int toget = need + i;
             if (inB.count(toget)) {
-                return vector<int>{A[i], toget};
+                return vector<int>{i, toget};
             }
 
         }
@@ -66,23 +67,23 @@ public:
 };
 
 void test1() {
-    vector<int> v1{1,1};
-    vector<int> v2{2,2};
+    vector<int> v1{1, 1};
+    vector<int> v2{2, 2};
 
     cout << " [1,2] ? " << Solution().fairCandySwap(v1, v2) << endl;
 
-    vector<int> v3{1,2};
-    vector<int> v4{2,3};
+    vector<int> v3{1, 2};
+    vector<int> v4{2, 3};
 
     cout << " [1,2] ? " << Solution().fairCandySwap(v3, v4) << endl;
 
     vector<int> v5{2};
-    vector<int> v6{1,3};
+    vector<int> v6{1, 3};
 
     cout << " [2,3] ? " << Solution().fairCandySwap(v5, v6) << endl;
 
-    vector<int> v7{1,2,5};
-    vector<int> v8{2,4};
+    vector<int> v7{1, 2, 5};
+    vector<int> v8{2, 4};
 
     cout << " [5,4] ? " << Solution().fairCandySwap(v7, v8) << endl;
 }
